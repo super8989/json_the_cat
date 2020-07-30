80c64bc3cbcbb25dd97b9169e7cd5196ab002a5f
Sam Yang <samyang8989@gmail.com>
2020-07-30 18:18:31 +0000

    parse the data from the api request

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,8 +3,8 @@ const request = require('request');
 request(
 	'https://api.thecatapi.com/v1/breeds/search?q=sib',
 	(error, response, body) => {
-		// console.log('response', response);
-		// console.log('body', body);
-		console.log(typeof body);
+		const data = JSON.parse(body);
+		console.log(typeof data);
+		console.log(data[0].description);
 	}
 );
